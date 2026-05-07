9ec7d89ddf95eb9a0e605e1a926c85d736ffb9b2
safwanalbeshti <53099990+safwanalbeshti@users.noreply.github.com>
2026-05-07 13:07:20 -0600

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Guess the number!");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Equal => {
+                println!("Spot on!");
+                break;
+            },
+            Ordering::Greater => println!("Too high!"),
+        }
+    }
+}
